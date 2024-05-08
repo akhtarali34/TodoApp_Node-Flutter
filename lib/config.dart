5e263d888ccff92d0f92ad0664c9f8ad36e5d939
akhtarali34 <akhtar4430@gmail.com>
2024-05-08 11:00:17 +0500

    iitial commit

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+final url = 'http://192.168.43.81:5000/';
+final registration = url + "registeration";
+final login = url + 'login';
+final addtodo = url + 'storeTodo';
+final getToDoList = url + 'getUserTodoList';
+final deleteTodo = url + 'deleteTodo';
